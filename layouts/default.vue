da7becf906b5204ee9a47f3fca38deb20712da99
catajoynick <32421726+catajoynick@users.noreply.github.com>
2021-10-27 06:21:31 +0800

    init
    initialize new nuxt app

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style></style>
