8f99e1dd5744b732645835b879e206d15fd5b477
catajoynick <32421726+catajoynick@users.noreply.github.com>
2021-10-27 07:36:23 +0800

    Table Data
    Added table data

---
+++ pages/data-table.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="person"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Date",
+          value: "date"
+        },
+        {
+          text: "First Name",
+          align: "start",
+          sortable: false,
+          value: "firstName"
+        },
+        { text: "Middle Name", value: "middleName" },
+        { text: "Last Name", value: "lastName" },
+        { text: "Age", value: "age" }
+      ],
+      person: [
+        {
+          date: new Date().toDateString(),
+          firstName: "Nelson",
+          middleName: "Ylanan",
+          lastName: "Catajoy",
+          age: 24
+        }
+      ]
+    };
+  }
+};
+</script>
